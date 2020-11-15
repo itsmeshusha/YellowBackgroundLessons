b25c22a3372e0987e90a254d007ee488c6d9026b
Alexandra <alexbushueva27@gmail.com>
2020-11-15 19:04:42 +0300

    FLUX

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 
 
 
@@ -11,7 +11,6 @@ export const uncontrolledInput = () => {
     return <input />
 }
 
-
 export const getValueOfUncontrolledInputByButtonPress = () => {
     const [value, setValue] = useState("")
     const inputRef = useRef<HTMLInputElement>(null)
@@ -22,7 +21,6 @@ export const getValueOfUncontrolledInputByButtonPress = () => {
     } }> save</button> actual value: {value}
     </>
 }
-
 
 export const uncontrolledInputWithTrackingValue = () => {
     const [value, setValue] = useState("")
@@ -35,5 +33,36 @@ export const uncontrolledInputWithTrackingValue = () => {
     </>
 }
 
-export const controlledInput = () => <input value={'hello'} />
+export const controlledInput = () => {
+    const [parentValue, setParentValue] = useState("")
+    const onChangeHandle = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandle} />
+}
 
+export const controlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+
+    const onChangeHandle = (e: ChangeEvent<HTMLInputElement>) => {
+
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type={'checkbox'} checked={parentValue} onChange={onChangeHandle} />
+}
+
+export const controlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+    const onChangeHandle = (e: ChangeEvent<HTMLSelectElement>) => {
+
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <select value={parentValue} onChange = {onChangeHandle} >
+        <option>none</option>
+        <option value={'1'}>Moscow</option>
+        <option value={'2'}>Minsk</option>
+        <option value={'3'}>Kiev</option>
+
+    </select>
+}
