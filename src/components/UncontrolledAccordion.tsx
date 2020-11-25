9ed5bb0c9a10e4cfa0f2d3b29c806b6d806a5372
Alexandra <alexbushueva27@gmail.com>
2020-11-25 22:54:17 +0300

    useReducer

--- src/components/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 
 type AccordionPropsType = {
     title: string
@@ -7,25 +7,53 @@ type AccordionPropsType = {
 
 type AccordionTitlePageType = {
     title: string
-    setCollapsed: (collapsed: boolean) => void
+    setCollapsed: () => void
+
+}
+
+type ActionType = {
+    type: string
+}
+
+export type StateType = {
     collapsed: boolean
+}
+
+export const TOGGLE_COLLAPSED = "TOGGLE-COLLAPSED"
+
+export const reducer = (state: StateType, action: ActionType): StateType => {
+    switch (action.type) {
+        case TOGGLE_COLLAPSED:
+            return {
+                ...state,
+                collapsed: !state.collapsed
+            }
+        default:
+            return state;
+    }
 }
 
 
 export function UncontrolledAccordion(props: AccordionPropsType) {
 
-    let [collapsed, setCollapsed] = useState(false)
+
+
+    //let [collapsed, setCollapsed] = useState(false)
+    const [state, dispatch] = useReducer(reducer, {collapsed: false})
 
     return <div>
-        <AccordionTitle title={props.title} setCollapsed={setCollapsed} collapsed={collapsed}/>
 
-        { !collapsed && <AccordionBody/> }
+        <AccordionTitle title={props.title} setCollapsed={() => dispatch({
+            type: TOGGLE_COLLAPSED
+        })} />
+
+        { !state.collapsed && <AccordionBody/> }
     </div>
 
 }
 
 export function AccordionTitle (props: AccordionTitlePageType) {
-    return <div onClick={() => {props.setCollapsed(!props.collapsed)}}>
+    return <div onClick={() => {props.setCollapsed()}}>
         <h3>{props.title}</h3>
     </div>
 }
